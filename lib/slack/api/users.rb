e18a3917b22b433b50462fb0c44632760abeab21
Pete Varley <petevarley@gmail.com>
2014-11-18 11:19:34 -0500

    Update users.rb

--- lib/slack/api/users.rb
+++ lib/slack/api/users.rb
@@ -1,6 +1,12 @@
 module Slack
   module API
     class Users < Slack::API::Base
+      def info(args = {})
+      	fail ArgumentError, 'user is required' unless args.key?(:user)
+      	
+      	get('users.list', args)
+      end
+      	
       def list(args = {})
         get('users.list', args)
       end
